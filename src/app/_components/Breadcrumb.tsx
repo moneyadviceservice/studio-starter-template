8cab292ebc7be35d7491e24e94f3d111003d15c8
chrisj-skinner <c.skinner1@me.com>
2025-05-08 15:03:41 +0100

    feat: update Breadcrump styling

--- src/app/_components/Breadcrumb.tsx
+++ src/app/_components/Breadcrumb.tsx
@@ -13,11 +13,11 @@ export const Breadcrumb = ({ breadcrumbs }: Props) => (
       <ol className="cmp-breadcrumb__list flex gap-2">
         {breadcrumbs.map((breadcrumb, index) => (
           <li key={index} className="cmp-breadcrumb__list-item">
-            <a href={breadcrumb.url} className="cmp-breadcrumb__list-item-link">
+            <a href={breadcrumb.url} className="cmp-breadcrumb__list-item-link hover:underline font-bold text-pink-600">
               <span>{breadcrumb.title}</span>
             </a>
             {index < breadcrumbs.length - 1 && (
-              <span className="cmp-breadcrumb__separator pl-2">/</span>
+              <span className="cmp-breadcrumb__separator pl-2 font-bold text-pink-600">&gt;</span>
             )}
           </li>
         ))}
